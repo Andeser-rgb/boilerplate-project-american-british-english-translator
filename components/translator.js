8401673885a25c8ae5862ebd47f8efb5465c3da0
Andeser-rgb <andeser@protonmail.com>
2021-08-06 08:15:46 +0200

    Added management to post request /api/translate

--- components/translator.js
+++ components/translator.js
@@ -4,6 +4,61 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+    americanToBritish(text) {
+        if (!text) return text;
+
+        let copy = text;
+        for (let key in americanOnly)
+            if (copy.indexOf(key) !== -1)
+                copy = copy.replace(key, americanOnly[key]);
+
+        return copy
+            .split(' ')
+            .map(d => {
+                const newD = d.toLowerCase();
+                return /\d{1,2}:\d{2}/.test(newD) ?
+                    newD.replace(':', '.') :
+                    americanToBritishSpelling.hasOwnProperty(newD) ?
+                    americanToBritishSpelling[newD] :
+                    americanToBritishTitles.hasOwnProperty(newD) ?
+                    americanToBritishTitles[newD].replace(/^\w/, c => c.toUpperCase()) :
+                    d;
+            })
+            .join(' ');
+    }
+
+    britishToAmerican(text) {
+        if (!text) return text;
+
+        let copy = text;
+        const britishToAmericanSpelling = this.invertObj(americanToBritishSpelling);
+        const britishToAmericanTitles = this.invertObj(americanToBritishTitles);
+
+        for (let key in britishOnly)
+            if (copy.includes(key))
+                copy = copy.replace(key, britishOnly[key]);
+
+        return copy
+            .split(' ')
+            .map(d => {
+                const newD = d.toLowerCase();
+                return /\d{1,2}[.]\d{2}/.test(newD) ?
+                    newD.replace('.', ':') :
+                    britishToAmericanSpelling.hasOwnProperty(newD) ?
+                    britishToAmericanSpelling[newD] :
+                    britishToAmericanTitles.hasOwnProperty(newD) ?
+                    britishToAmericanTitles[newd].replace(/^\w/, c => c.toUpperCase()) :
+                    d;
+            })
+            .join(' ');
+    }
+
+    invertObj(obj) {
+        let invObj = {};
+        for (let key in obj)
+            invObj[obj[key]] = key;
+        return invObj;
+    }
 
 }
 
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,52 @@
 'use strict';
+/*jshint esversion: 6, node: true */
 
 const Translator = require('../components/translator.js');
 
-module.exports = function (app) {
-  
-  const translator = new Translator();
+module.exports = function(app) {
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+    const translator = new Translator();
+
+    app.route('/api/translate')
+        .post((req, res) => {
+            const {
+                text,
+                locale
+            } = req.body;
+
+            if (!text || !locale) {
+                res.json({
+                    error: 'Required field(s) missing'
+                });
+                return;
+            }
+            let translatedText = 'ciccio';
+            let textWords = text.split(' ');
+            if (locale === 'american-to-british') {
+                translatedText = translator.americanToBritish(text)
+                    .split(' ')
+                    .map(d => {
+                        return textWords.includes(d) ? d : spanEncapsulate(d);
+                    })
+                    .join(' ');
+            } else if (locale === 'british-to-american') {
+                translatedText = translator.britishToAmerican(text)
+                    .split(' ')
+                    .map(d => {
+                        return textWords.includes(d) ?
+                            spanEncapsulate(d) :
+                            d;
+                    })
+                    .join(' ');
+            }
+            res.json({
+                text: text,
+                translation: translatedText
+            });
+
+        });
 };
+
+function spanEncapsulate(text) {
+    return '<span class="highlight">' + text + '</span>';
+}
